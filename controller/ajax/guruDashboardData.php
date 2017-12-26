cf7c301bdedf23e351a3de89bef3fdd18a81b00e
Tisna <wayantisna74@gmail.com>
2017-12-26 08:55:38 +0800

    guru dashboard 1

--- controller/ajax/guruDashboardData.php
+++ controller/ajax/guruDashboardData.php
@@ -1,4 +1,36 @@
 <?php 
 include '../../connection.php';
 
+function getDataForChart($connect,$const) {
+	$id_kelas 			= $_POST['id_kelas'];
+	$id_tahun_ajaran 	= $_POST['id_tahun_ajaran'];
+	$start_date 		= $_POST['start_date'];
+	$end_date			= $_POST['end_date'];
+	$query = '
+		SELECT 
+		COUNT(tb_perkembangan.id),
+		SUM($const="A") as "'.$const.'"_a, 
+		SUM($const="B") as "'.$const.'"_b, 
+		SUM($const="C") as "'.$const.'"_c 
+		FROM tb_perkembangan 
+		LEFT JOIN tb_siswa ON tb_perkembangan.nis = tb_siswa.nis
+		LEFT JOIN tb_detail_siswa ON tb_detail_siswa.id_siswa = tb_siswa.id
+		LEFT JOIN tb_tahun_ajaran ON tb_tahun_ajaran.id = tb_detail_siswa.id_tahun_ajaran
+		LEFT JOIN tb_kelas ON tb_kelas.id = tb_detail_siswa.id_kelas
+		WHERE tb_siswa.nis IS NOT NULL AND tb_detail_siswa.id_kelas = :id_kelas AND tb_detail_siswa.id_tahun_ajaran = :id_tahun_ajaran AND tgl BETWEEN :start_date AND :end_date ';
 
+	$statement = $this->db->query($query);
+	$statement->execute(array(
+		':id_kelas'			=> $id_kelas,
+		':id_tahun_ajaran'	=> $id_tahun_ajaran,
+		':start_date'		=> $start_date,
+		':end_date'			=> $end_date
+	));
+
+	$data = array();
+	$result = $statement->fetchAll();
+	foreach ($result as $row) {
+		$data[] = $row;
+	}
+	echo json_encode($data);
+}
--- dashboard/dashboard-guru.php
+++ dashboard/dashboard-guru.php
@@ -35,12 +35,89 @@
 
   <div class="col-lg-12 col-xs-12">
     <!-- small box -->
-    <div class="box">
+    <div>
       <div class="box-header">
         <p>Dashboard Guru</p>
       </div>
       <div class="box-body">
-        <i class="ion ion-bag"></i>
+        <div class="row">
+
+          <div class="col-md-6">
+            <div class="box box-success">
+              <div class="box-header with-border">
+                <h3 class="box-title">Motorik</h3>
+
+                <div class="box-tools pull-right">
+                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+              </div>
+              <div class="box-body">
+                <div class="chart">
+                  <canvas id="barChartMotorik" style="height:230px"></canvas>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-md-6">
+            <div class="box box-info">
+              <div class="box-header with-border">
+                <h3 class="box-title">Daya Ingat</h3>
+                <div class="box-tools pull-right">
+                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+              </div>
+              <div class="box-body">
+                <div class="chart">
+                  <canvas id="barChartDayaIngat" style="height:230px"></canvas>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="box box-danger">
+              <div class="box-header with-border">
+                <h3 class="box-title">Keaktifan</h3>
+
+                <div class="box-tools pull-right">
+                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+              </div>
+              <div class="box-body">
+                <div class="chart">
+                  <canvas id="barChartKeaktifan" style="height:230px"></canvas>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="box box-primary">
+              <div class="box-header with-border">
+                <h3 class="box-title">Sosialisasi</h3>
+
+                <div class="box-tools pull-right">
+                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+              </div>
+              <div class="box-body">
+                <div class="chart">
+                  <canvas id="barChartSosialisasi" style="height:230px"></canvas>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
@@ -48,3 +125,28 @@
 
 
 <?php require 'partials/footer.php'; ?>
+<script type="text/javascript">
+  $('#tampilkan_siswa').click(function(){
+    var kelas = $('#kelas').val();
+    var tahun_ajaran = $('#tahun_ajaran').val();
+    var tgl_perkembangan_mulai = $('#tgl_perkembangan_mulai').val();
+    var tgl_perkembangan_akhir = $('#tgl_perkembangan_akhir').val();
+    if (tgl_perkembangan_mulai != '' && tgl_perkembangan_akhir != '') {
+      $('#perkembanganTable').DataTable().destroy();
+      fetchData('yes',kelas,tahun_ajaran,tgl_perkembangan_mulai,tgl_perkembangan_akhir);
+    }else{
+      alert("Tanggal diperlukan untuk pencarian data");
+    }
+  })
+
+  function fetchData(isSearch,idKelas,idTahunAjaran,startDate,endDate) {
+    $.ajax({
+      url: '../controller/ajax/guruDashboardData.php',
+      data: {isSearch:isSearch,idKelas:idKelas,idTahunAjaran:idTahunAjaran,startDate:startDate,endDate:endDate},
+      method: 'POST',
+      success: function(data){
+
+      }
+    });
+  }
+</script>
